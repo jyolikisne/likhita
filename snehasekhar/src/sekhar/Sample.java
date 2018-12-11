a4c658e52d44f8e2840e92beaf7fd34b2cb6f043
cubic <cubic@192.168.0.40>
2018-12-11 10:47:20 +0530

    pojkfsh

---
+++ snehasekhar/src/sekhar/Sample.java
@@ -0,0 +1,17 @@
+package sekhar;
+
+public class Sample {
+
+	public static void main(String[] args)
+	{
+	int a=5;
+	int b=6;
+	int c=89;
+	
+int d=(b*c);
+int f=(c+a);
+System.out.println("d");
+System.out.println("f");
+	}
+
+}
